45ac6863972bf55a30a3a29a4bdf5954a15a4526
jeon3029 <jeon3029@naver.com>
2023-09-03 19:40:37 +0900

    add contents

--- spring_cloud/gateway/firstservice/src/main/java/com/example/firstservice/FirstController.java
+++ spring_cloud/gateway/firstservice/src/main/java/com/example/firstservice/FirstController.java
@@ -1,15 +1,26 @@
 package com.example.firstservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
 @RestController
 @RequestMapping("/first-service")
-@Slf4j
 public class FirstController {
+    Environment env;
+
+    @Autowired
+    public FirstController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("/welcome")
     public String hello(){
         return "this is first service";
@@ -23,7 +34,8 @@ public class FirstController {
     }
 
     @GetMapping("/check")
-    public String check(){
-        return "this is first service CHECK";
+    public String check(HttpServletRequest req){
+        log.info("Server port : {}",req.getServerPort());
+        return String.format("this is first service CHECK port : %s",env.getProperty("local.server.port"));
     }
 }
